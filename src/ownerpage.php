9117604051bc77bc4024a1422eb23b693591a747
jagathi <jagathi@student.ubc.ca>
2024-11-23 19:04:35 -0800

    Add review form added to ownerpage

--- src/ownerpage.php
+++ src/ownerpage.php
@@ -9,8 +9,8 @@ error_reporting(E_ALL);
 // Set some parameters
 
 // Database access configuration
-$config["dbuser"] = "ora_jagathi";			// change "cwl" to your own CWL
-$config["dbpassword"] = "a81887028";	// change to 'a' + your student number
+$config["dbuser"] = "ora_cwl";			// change "cwl" to your own CWL
+$config["dbpassword"] = "a12345678";	// change to 'a' + your student number
 $config["dbserver"] = "dbhost.students.cs.ubc.ca:1522/stu";
 $db_conn = NULL;	// login credentials are used in connectToDB()
 
@@ -97,6 +97,38 @@ function connectToDB()
 <html>
 <head> 
     <title> Owner Dashboard </title>
+    <style>
+        table {
+            border-collapse: collapse;
+            margin: 0 auto;
+        }
+        table, th, td {
+            border: 1px solid black;
+
+        }
+
+        th, td {
+            padding: 8px;
+        }
+
+        .hidden {
+            display: none;
+        }
+        .button-container {
+            margin: 10px;
+            text-align: center;
+            position: absolute;
+            
+        }
+
+        #add_review_form {
+            margin-top: 20px; 
+            position: absolute;
+        
+        }
+
+    </style>
+        
 </head>
 <body>
     <div style="text-align: center;">
@@ -140,16 +172,80 @@ function connectToDB()
             </tbody>
         </table>
     </div>
+
         
         <div class = "button-container">
-
-        <form method="GET" action="addReview.php">
-            <input type="hidden" name="Owner_ID" value="<?php echo htmlentities($owner_id); ?>">
-            <button type="submit">Add Review</button>
-        </form>
+            <button onclick="toggleForm()"> Add Review </button>
         </div>
+
+        <div id="add_review_form" class="hidden" style="text-align: center;">
+        <h3>Add a Review</h3>
+        <form method="POST" action="">
+            <label for="Review_ID">Review ID:</label>
+            <input type="text" id="review_id" name="review_id" required><br><br>
+
+            <label for="Review_Date">Review Date:</label>
+            <input type="date" id="review_date" name="review_date" required><br><br>
+
+            <label for="Review_Rating">Review Rating (1-5):</label>
+            <input type="number" id="review_rating" name="review_rating" min="1" max="5" required><br><br>
+
+            <label for="Review_Comment">Review Comment:</label><br>
+            <textarea id="review_comment" name="review_comment" rows="4" cols="50" required></textarea><br><br>
+
+            <input type="hidden" name="owner_id" value="<?php echo isset($owner_id) ? htmlentities($owner_id) : ''; ?>">
+            <input type="hidden" name="owner_phone" value="<?php echo isset($owner_phone) ? htmlentities($owner_phone) : ''; ?>">
+
+            <button type="submit" name="addReviewSubmit">Submit Review</button>
+        </form>
+    </div>
+
+    <script>
+        function toggleForm() {
+            var form = document.getElementById("add_review_form");
+            if (form.classList.contains("hidden")) {
+                form.classList.remove("hidden");
+            } else {
+                form.classList.add("hidden");
+            }
+        }
+    </script>
       
 </body>
 </html>
 
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['addReviewSubmit'])) {
+    $Review_ID = $_POST['review_id'];
+    $Review_Date = $_POST['review_date'];
+    $Review_Rating = $_POST['review_rating'];
+    $Review_Comment = $_POST['review_comment'];
+    $Owner_ID = $_POST['owner_id'];
+    $Owner_Phone = $_POST['owner_phone'];
 
+    if (connectToDB()) {
+       
+        $query = "INSERT INTO Review (Review_ID, Review_Date, Review_Rating, Review_Comment, Owner_ID, Owner_Phone)
+                  VALUES (:review_id, :review_date, :review_rating, :review_comment, :owner_id, :owner_phone)";
+        $statement = oci_parse($db_conn, $query);
+
+        oci_bind_by_name($statement, ":review_id", $Review_ID);
+        oci_bind_by_name($statement, ":review_date", $Review_Date);
+        oci_bind_by_name($statement, ":review_rating", $Review_Rating);
+        oci_bind_by_name($statement, ":review_comment", $Review_Comment);
+        oci_bind_by_name($statement, ":owner_id", $Owner_ID);
+        oci_bind_by_name($statement, ":owner_phone", $Owner_Phone);
+        
+        
+
+        if (oci_execute($statement)) {
+            echo "<p style='color:green; text-align:center;'>Review added successfully!</p>";
+        } else {
+            $e = oci_error($statement);
+            echo "<p style='color:red; text-align:center;'>Error adding review: " . htmlentities($e['message']) . "</p>";
+        }
+
+        disconnectFromDB();
+    }
+}
+?>
